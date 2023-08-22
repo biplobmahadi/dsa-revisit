1a391a0cf4d0f4a1f453e3ae66b169e562ae8f1c
biplob <biplobhasan547@gmail.com>
2023-08-22 23:52:37 +0600

    merge 2 sorted array

---
+++ array/mergeTwoSorted.py
@@ -0,0 +1,29 @@
+def mergeTwoSorted(arr1, arr2):
+    len1 = len(arr1)
+    len2 = len(arr2)
+    if len1 == 0: return arr2
+    if len2 == 0: return arr1
+
+    i, j, newArr = 0, 0, []
+
+    while i < len1 or j < len2 :
+        if i < len1 and j < len2:
+            if arr1[i] > arr2[j]:
+                newArr.append(arr2[j])
+                j+=1
+            else:
+                newArr.append(arr1[i])
+                i+=1
+        elif j < len2:
+            newArr.append(arr2[j])
+            j+=1
+        else: 
+            newArr.append(arr1[i])
+            i+=1
+            
+    return newArr
+
+
+arr1 = []
+arr2 = [2, 4, 6]
+print(mergeTwoSorted(arr1, arr2))
