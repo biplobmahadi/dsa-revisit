49b810b459463c41d1281ab05a521fcdf7de5c8a
biplob <biplobhasan547@gmail.com>
2023-08-23 17:36:27 +0600

    create own singly linked list with some methods

---
+++ linkedList/mySinglyLL.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+class MySinglyLL: 
+    def __init__(self, val):
+        node = Node(val)
+        self.head = node
+        self.tail = self.head
+        self.length = 1
+
+    def print(self):
+        list = [] 
+        currentNode = self.head
+
+        while currentNode is not None:
+            list.append(currentNode.value)
+            currentNode = currentNode.next
+        return list
+    
+    def append(self, val):
+        node = Node(val)
+        self.tail.next = node
+        self.tail = node
+        self.length +=1
+        return self.length
+
+    def prepend(self, val):
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+        self.length += 1
+        return self.length
+
+    def insert(self, idx, val):
+        if idx <= 0:
+            self.prepend(val)
+        elif idx >= self.length:
+            self.append(val)
+        else:
+            i, parent = 1, self.head
+            while i is not idx:
+                i += 1
+                parent = parent.next
+            node = Node(val)
+            node.next = parent.next
+            parent.next = node
+            self.length+= 1
+        return self.length
+
+    def remove(self, idx):
+        if(idx >= 0 and idx < self.length):
+            i, parent = 1, self.head
+            while i is not idx:
+                i += 1 
+                parent = parent.next
+            parent.next = parent.next.next
+            self.length -= 1
+        return self.length
+    
+    def __str__(self):
+        return f'len is: {self.length}'
+
+
+mysinglyLL = MySinglyLL(3)
+mysinglyLL.append(4)
+mysinglyLL.append(5)
+mysinglyLL.prepend(2)
+mysinglyLL.insert(-1, 8)
+mysinglyLL.insert(2, 84)
+mysinglyLL.insert(6, 843)
+mysinglyLL.remove(2)
+
+print(mysinglyLL)
+print(mysinglyLL.print())
