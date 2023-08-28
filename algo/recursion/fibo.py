76092d4b3bc4530b8f608083cfa240f4e93b665a
biplob <biplobhasan547@gmail.com>
2023-08-28 17:45:43 +0600

    add memorization of dp for fibo func

--- algo/recursion/fibo.py
+++ algo/recursion/fibo.py
@@ -13,9 +13,27 @@ def fiboIterative(n):
         return res
     
 print(fiboIterative(10))
-
+i = 0
+j = 0
 def fiboRecursive(n):
+    global i
+    i+=1
     if n < 2: return n
     return fiboRecursive(n-1) + fiboRecursive(n-2)
 
-print(fiboRecursive(10))
+print(fiboRecursive(30))
+
+def memoFib():
+    cache = {}
+    def fib(n):
+        global j
+        j+=1
+        if n<2: return n
+        if not cache.get(n):
+            cache[n] = fib(n-1) + fib(n-2)
+        return cache[n]
+    return fib
+
+fib = memoFib()
+print(fib(30))
+print(i, j)
