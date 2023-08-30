f4d3fdc42b09c106cd35bafe95615b2348463779
biplob <biplobhasan547@gmail.com>
2023-08-30 22:36:22 +0600

    add two sum brute force soln

---
+++ problems/array/twoSum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    for i in range(0, len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+nums = [3,3]
+target = 6
+print(twoSum(nums, target))
