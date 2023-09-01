2266b8f4770b6511d5e2e2d28ed94fdd1378b06b
biplob <biplobhasan547@gmail.com>
2023-09-01 11:14:19 +0600

    add optimal soln

--- problems/array/maxArea.py
+++ problems/array/maxArea.py
@@ -15,20 +15,20 @@ def maxArea(height: list):
 height = [1,8,6,2,5,4,8,3,7]
 print(maxArea(height))
 
-# def maxAreaOptimal(height):
-#     i, j, max = 0, len(height) - 1, 0
+def maxAreaOptimal(height):
+    i, j, max = 0, len(height) - 1, 0
 
-#     while i < j:
-#         width = j - i
-#         area = 0
-#         if height[i] < height[j]:
-#             area = height[i] * width
-#             i += 1
-#         else:
-#             area = height[j] * width
-#             j -= 1
-#         if area > max:
-#             max = area
-#     return max
+    while i < j:
+        width = j - i
+        area = 0
+        if height[i] < height[j]:
+            area = height[i] * width
+            i += 1
+        else:
+            area = height[j] * width
+            j -= 1
+        if area > max:
+            max = area
+    return max
 
-# print(maxAreaOptimal(height))
+print(maxAreaOptimal(height))
