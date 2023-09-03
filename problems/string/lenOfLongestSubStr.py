0b4ade50f5b16914cd1e8c00fc27f48eba4df99c
biplob <biplobhasan547@gmail.com>
2023-09-03 12:01:23 +0600

    add optimal soln

--- problems/string/lenOfLongestSubStr.py
+++ problems/string/lenOfLongestSubStr.py
@@ -13,3 +13,15 @@ def lenOfLongestSubStr(s):
 
 s = ' '
 print(lenOfLongestSubStr(s))
+
+def lenOfLongestSubStrOptimal(s):
+    left, longest, map, n = 0, 0, {}, len(s)
+    for right in range(0, n):
+        current = s[right]
+        seenInMap = map.get(current)
+        if seenInMap is not None and seenInMap >= left:
+            left = seenInMap + 1
+        longest = max(longest, right - left + 1)
+        map[current] = right
+    return longest
+print(lenOfLongestSubStrOptimal(s))
