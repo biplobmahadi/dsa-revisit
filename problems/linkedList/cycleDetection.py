fa97ac65dfbdbeb21dcfa7fba36c922308253961
biplob <biplobhasan547@gmail.com>
2023-09-04 23:25:43 +0600

    apply floy'd hare and tortoise algo to find cycle starting postion of linked list

--- problems/linkedList/cycleDetection.py
+++ problems/linkedList/cycleDetection.py
@@ -8,3 +8,24 @@ def cycleDetection(head):
         else:
             return current
     return None
+
+def cycleDetectionOptimal(head):
+    if head == None:
+        return None
+    hare = tortoise = head
+    count = 0
+    while (hare and hare != tortoise) or count < 1:
+        if count<1:
+            count+=1
+        tortoise = tortoise.next
+        if hare.next:
+            hare = hare.next.next
+        else:
+            hare = None
+    if hare:
+        begin = head
+        while begin != hare:
+            begin = begin.next
+            hare = hare.next
+        return hare
+    return None
