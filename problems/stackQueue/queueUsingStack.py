0d4492f70ad0f8a3e84acc115ebd9982914834a9
biplob <biplobhasan547@gmail.com>
2023-09-05 22:56:27 +0600

    add amortize soln

--- problems/stackQueue/queueUsingStack.py
+++ problems/stackQueue/queueUsingStack.py
@@ -25,3 +25,26 @@ class MyQueue:
     def empty(self):
         return len(self.stack1) == 0
 
+
+class MyQueueAmortized:
+    def __init__(self) -> None:
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        self.stack1.append(x)
+    
+    def pop(self):
+        self.peek()
+        return self.stack2.pop()
+    
+    def peek(self):
+        if not self.stack2:
+            for i in range(0, len(self.stack1)):
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+    
+    def empty(self):
+        return not (self.stack1 or self.stack2)
+    
+
