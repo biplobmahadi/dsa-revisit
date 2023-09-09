4d1249f3a081cc0594db60dbc184db43d98cd3ed
biplob <biplobhasan547@gmail.com>
2023-09-09 16:15:11 +0600

    count node in a complete binary tree solved with optimal time

--- problems/binaryTree/countNodes.py
+++ problems/binaryTree/countNodes.py
@@ -12,3 +12,42 @@ def countNodes(root):
         if popped.right:
             queue.append(popped.right)
     return count
+
+def countNodesOptimal(root):
+    if not root:
+        return 0
+    height = findHeight(root)
+    totalNodeWithoutLastLevel = pow(2, height-1) - 1
+    totalInLastLevel = getTotalInLastLevel(root, height)
+    return totalInLastLevel + totalNodeWithoutLastLevel
+
+def findHeight(root):
+    current = root
+    count = 0
+    while current:
+        count+=1
+        current = current.left
+    return count
+
+def getTotalInLastLevel(root, height):
+    left, right = 0, pow(2, height-1) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        midNode = findAnyNode(mid, root, height)
+        if midNode:
+            left = mid+1
+        else:
+            right = mid -1
+    return right + 1
+
+def findAnyNode(position, root, height):
+    left, right, current = 0, pow(2, height-1) - 1, root
+    while left != right:
+        mid = (left + right) // 2
+        if position <= mid:
+            current = current.left
+            right = mid
+        else:
+            current = current.right
+            left = mid + 1
+    return current
