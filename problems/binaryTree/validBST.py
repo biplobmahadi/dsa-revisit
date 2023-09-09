65d06eb49e82676a40d3496b9837ccdcdc1ebfaa
biplob <biplobhasan547@gmail.com>
2023-09-09 22:15:07 +0600

    add optimal soln for valid bst or not

--- problems/binaryTree/validBST.py
+++ problems/binaryTree/validBST.py
@@ -17,3 +17,21 @@ def isValidBST(root):
         i+=1
         j+=1
     return True
+
+import math
+
+def isValidBSTOptimal(root):
+    if not root:
+        return True
+    return dfsPre(root, -math.inf, math.inf)
+
+def dfsPre(root, greater, lesser):
+    if not (root.val > greater and root.val < lesser):
+        return False
+    if root.left:
+        if not (dfsPre(root.left, greater, root.val)):
+            return False
+    if root.right:
+        if not (dfsPre(root.right, root.val, lesser)):
+            return False
+    return True
