0704687cf0f31e1d90eabc58bc8a5fe4275d4c92
biplob <biplobhasan547@gmail.com>
2023-09-12 10:22:11 +0600

    add dfs in graph with adjacency matrix

--- graph/dfs.py
+++ graph/dfs.py
@@ -29,14 +29,14 @@ def dfsAdjacencyList(graph, list, seen, curr):
         dfsAdjacencyList(graph, list, seen, i)
     return list
 
-# def dfsAdjacencyMatrix(graph, list, seen, curr):
-#     if seen.get(curr): return
-#     list.append(curr)
-#     seen[curr] = True
-#     for i in range(len(graph[curr])):
-#         if graph[curr][i] == 1:
-#             dfsAdjacencyMatrix(graph, list, seen, i)
-#     return list
+def dfsAdjacencyMatrix(graph, list, seen, curr):
+    if seen.get(curr): return
+    list.append(curr)
+    seen[curr] = True
+    for i in range(len(graph[curr])):
+        if graph[curr][i] == 1:
+            dfsAdjacencyMatrix(graph, list, seen, i)
+    return list
 
-# print(dfsAdjacencyList(graph, [], {}, 0))
-# print(dfsAdjacencyMatrix(graphInMatrix, [], {}, 0))
+print(dfsAdjacencyList(graph, [], {}, 0))
+print(dfsAdjacencyMatrix(graphInMatrix, [], {}, 0))
