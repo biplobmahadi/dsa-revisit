80ebf27938b7c481d0e78b210cc5654295cf18f3
biplob <biplobhasan547@gmail.com>
2023-09-21 10:38:23 +0600

    memorize the repeated val in dp

--- problems/dp/minCost.py
+++ problems/dp/minCost.py
@@ -8,3 +8,18 @@ def minCost(i, cost):
     return cost[i] + min(minCost(i-1, cost), minCost(i-2, cost))
 
 print(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))
+
+def minCostClimbingStairsOpti(cost):
+    n = len(cost)
+    memo = [None] * n
+    memo[0] = cost[0]
+    memo[1] = cost[1]
+    return min(minCostOpti(n-1, cost, memo), minCostOpti(n-2, cost, memo))
+
+def minCostOpti(i, cost, memo):
+    if i < 0: return 0
+    if memo[i]: return memo[i]
+    memo[i] = cost[i] + min(minCostOpti(i-1, cost, memo), minCostOpti(i-2, cost, memo))
+    return memo[i]
+
+print(minCostClimbingStairsOpti([1,100,1,1,1,100,1,1,100,1]))
