db183e6be525789d715ca37015d08caf0bf17a11
biplob <biplobhasan547@gmail.com>
2023-10-04 22:02:52 +0600

    add brute force soln for largest sum of subarr

---
+++ algo/kadanes.py
@@ -0,0 +1,25 @@
+# prob: given an int arr, find the largest sum of subarr
+
+nums = [4, -1, 2, -7, 3, 4]
+
+def brute(nums):
+    maxSum = nums[0]
+    for i in range(len(nums)):
+        current = 0
+        for j in range(i, len(nums)):
+            current += nums[j]
+            maxSum = max(maxSum, current)
+    return maxSum
+
+print(brute(nums))
+
+# def kadanes(nums):
+#     maxSum = nums[0]
+#     current = 0
+#     for n in nums:
+#         current += n
+#         current = max(current, 0)
+#         maxSum = max(maxSum, current)
+#     return maxSum
+
+# print(kadanes(nums))
