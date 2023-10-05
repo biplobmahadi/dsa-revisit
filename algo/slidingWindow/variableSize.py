4bcaa6e0dbc7de26875ed4116ed44c4eda91ba0d
biplob <biplobhasan547@gmail.com>
2023-10-05 08:34:25 +0600

    solve min len subarr, where sum is greater or equal to target using variable sliding window

--- algo/slidingWindow/variableSize.py
+++ algo/slidingWindow/variableSize.py
@@ -12,3 +12,19 @@ def longestSubarr(nums):
     return long
 
 print(longestSubarr(nums))
+
+# given positive int arr, 
+# find min len subarr, where sum is greater or equal to target
+
+def minLenSubarr(nums, target):
+    total, lenght, l = 0, len(nums), 0
+    for r in range(len(nums)):
+        total += nums[r]
+        while total >= target:
+            lenght = min(lenght, r-l+1)
+            total -= nums[l]
+            l += 1
+    return lenght
+
+arr = [2, 3, 1, 2, 4, 3]
+print(minLenSubarr(arr, 6))
