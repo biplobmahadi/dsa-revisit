b802b392f4ddc7dbc6d629886a14bbfc49b71e86
biplob <biplobhasan547@gmail.com>
2024-01-06 00:38:28 +0600

    add bottom up approach

--- algo/dp/twoDimensional.py
+++ algo/dp/twoDimensional.py
@@ -22,4 +22,14 @@ def memo(r, c, row, col, cache):
 
 print(memo(0, 0, 4, 4, [[0]*4 for _ in range(4)]))
 
+def dp(row, col):
+    prev = [0] * col
+    for _ in range(row-1, -1, -1):
+        curr = [0] * col
+        curr[col-1] = 1
+        for c in range(col-2, -1, -1):
+            curr[c] = curr[c+1] + prev[c]
+        prev = curr
+    return prev[0]
 
+print(dp(4, 4))
