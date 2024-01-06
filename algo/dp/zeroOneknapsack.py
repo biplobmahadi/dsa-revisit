5d2ced9dd67108fe3992658357ec1c9315227bca
biplob <biplobhasan547@gmail.com>
2024-01-06 23:01:11 +0600

    add memorization

--- algo/dp/zeroOneknapsack.py
+++ algo/dp/zeroOneknapsack.py
@@ -15,3 +15,22 @@ def rec(i, p, w, c):
     return maxProfit
 
 print(topDown([4, 4, 7, 1], [5, 2, 3, 1], 8))
+
+def topDownMemo(profit, weight, capacity):
+    cache = [[-1] * (capacity+1) for _ in range(len(profit))]
+    return recMemo(0, profit, weight, capacity, cache)
+
+def recMemo(i, p, w, c, cache):
+    if i == len(p):
+        return 0
+    if cache[i][c] != -1:
+        return cache[i][c]
+    
+    cache[i][c] = recMemo(i+1, p, w, c, cache)
+    newC = c - w[i]
+    if newC >=0:
+        inc = recMemo(i+1, p, w, newC, cache) + p[i]
+        cache[i][c] = max(inc, cache[i][c])
+    return cache[i][c]
+
+print(topDownMemo([4, 4, 7, 1], [5, 2, 3, 1], 8))
