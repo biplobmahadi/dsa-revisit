c3371caa15c38024db443c826bdaf7b709e120f1
Biplob <biplob.kazsoft@gmail.com>
2024-10-02 12:45:30 +0600

    change implementation of segment tree

--- 3Topic/ds/segmentTree.py
+++ 3Topic/ds/segmentTree.py
@@ -1,4 +1,4 @@
-class SegmentTree:
+class Node:
     def __init__(self, total = 0, l=0, r=0) -> None:
         self.sum = total
         self.l = l
@@ -6,35 +6,38 @@ class SegmentTree:
         self.left = None
         self.right = None
 
-    @staticmethod
+class SegmentTree:
+    def __init__(self) -> None:
+        self.root = None
+
     def build(self, nums, l, r):
         if l == r: 
-            return SegmentTree(nums[l], l, r)
+            return Node(nums[l], l, r)
 
-        root = SegmentTree(0, l, r)
+        root = Node(0, l, r)
         m = (l + r) //2
-        root.left = SegmentTree.build(nums, l, m)
-        root.right = SegmentTree.build(nums, m+1, r)
+        root.left = self.build(nums, l, m)
+        root.right = self.build(nums, m+1, r)
         root.sum = root.left.sum + root.right.sum
         return root
     
-    def update(self, i, v):
-        if self.l == self.r:
-            self.sum = v
+    def update(self, i, v, root):
+        if root.l == root.r:
+            root.sum = v
             return
-        m = (self.r + self.l)//2
-        if i > m: self.right.update(i, v)
-        else: self.left.update(i, v)
-        self.sum = self.left.sum + self.right.sum
+        m = (root.r + root.l)//2
+        if i > m: self.update(i, v, root.right)
+        else: self.update(i, v,root.left)
+        root.sum = root.left.sum + root.right.sum
 
-    def rangeSum(self, l, r):
-        if self.l == l and self.r == r: return sum
-        m = (self.r + self.l)//2
-        if r <= m: return self.left.rangeSum(l, r)
-        if l > m: return self.right.rangeSum(l, r)
-        else: return self.left.rangeSum(l, r) + self.right.rangeSum(l, r)
+    def rangeSum(self, l, r, root):
+        if root.l == l and root.r == r: return root.sum
+        m = (root.r + root.l)//2
+        if r <= m: return self.rangeSum(l, r, root.left)
+        if l > m: return self.rangeSum(l, r, root.right)
+        else: return self.rangeSum(l, m, root.left) + self.rangeSum(m+1, r, root.right)
 
 sg = SegmentTree()
-
-sg.build([5, 3, 7, 1, 4, 2], 0, 5)
-# sg.rangeSum(3, 5)
+sg.root = sg.build([5, 3, 7, 1, 4, 2], 0, 5)
+print(sg.root.l, sg.root.r)
+print(sg.rangeSum(1, 2, sg.root))
