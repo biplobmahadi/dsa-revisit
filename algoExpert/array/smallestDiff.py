36b261d1de4b68a9b18d23c7b21417202d5a6f10
biplob <biplobhasan547@gmail.com>
2024-10-23 23:13:37 +0600

    add optimal soln to find smallest diff

--- algoExpert/array/smallestDiff.py
+++ algoExpert/array/smallestDiff.py
@@ -11,4 +11,26 @@ def smallestDifference(arrayOne, arrayTwo):
                 currentDiff = distance
                 pair = [one, two]
     return pair
+
+
+def smallestDifference2(arrayOne, arrayTwo):
+    arrayOne.sort()
+    arrayTwo.sort()
+
+    i, j = 0, 0
+    currDiff, res = math.inf, [0, 0]
+
+    while i < len(arrayOne) and j < len(arrayTwo):
+        one, two = arrayOne[i], arrayTwo[j]
+        distance = abs(one-two)
+        if one<two:
+            i+=1
+        elif two<one:
+            j+=1
+        else: 
+            return [one, two]
+        if distance < currDiff:
+            currDiff = distance
+            res = [one, two]
+    return res
     
