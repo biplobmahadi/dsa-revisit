566d6327505bb2186c202ce5fd62193f380f047c
Biplob <biplobhasan547@gmail.com>
2024-10-24 18:55:21 +0600

    add bottom up approach

--- blind75/dp/climbStair.py
+++ blind75/dp/climbStair.py
@@ -8,3 +8,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         dp = {}
         return climb(n, dp)
+    
+class Solution2:
+    def climbStairs2(self, n: int) -> int:
+        if n <= 2: return n
+        dpOne, dpTwo = 1, 2
+        for i in range(3, n+1):
+            dpOne, dpTwo = dpTwo, dpOne+dpTwo
+        return dpTwo
