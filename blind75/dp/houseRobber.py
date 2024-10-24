3d6c5d35311d436f0a3a00c9f9a15b169db6f147
Biplob <biplobhasan547@gmail.com>
2024-10-24 18:56:37 +0600

    house robber bottom up

--- blind75/dp/houseRobber.py
+++ blind75/dp/houseRobber.py
@@ -7,3 +7,10 @@ class Solution:
                 dp[i] = max(nums[i]+robbing(i-2), robbing(i-1))
             return dp[i]
         return robbing(len(nums)-1)
+
+class Solution:
+    def rob(self, nums) -> int:
+        dpOne, dpTwo = 0, nums[0]
+        for i in range(1, len(nums)):
+            dpOne, dpTwo = dpTwo, max(nums[i]+dpOne, dpTwo)
+        return dpTwo
