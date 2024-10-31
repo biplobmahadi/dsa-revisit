572b76b1c9d8f6f3caef6f2892cb10cdbf53fba7
Biplob <biplobhasan547@gmail.com>
2024-10-31 17:26:10 +0600

    add bottom up approach to find decode ways

--- blind75/dp/decodeWays.py
+++ blind75/dp/decodeWays.py
@@ -11,3 +11,15 @@ class Solution:
                     dp[i] += goo(i+2)
             return dp[i]
         return goo(0)
+    
+class Solution2:
+    def numDecodings(self, s: str) -> int:
+        dp = {}
+        dp[len(s)] = 1
+        for i in reversed(range(len(s))):
+            dp[i] = 0
+            if s[i] != '0':
+                dp[i] += dp[i+1]
+            if (i+1)<len(s) and ((s[i] == '1') or (s[i] == '2' and s[i+1] in '0123456')):
+                dp[i] += dp[i+2]
+        return dp[0]
