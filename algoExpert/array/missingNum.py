23a37f2fc45816a8337f671e402b9ef640ad2a73
biplob <biplobhasan547@gmail.com>
2024-11-06 23:49:59 +0600

    find missing nums in array with constant memory

--- algoExpert/array/missingNum.py
+++ algoExpert/array/missingNum.py
@@ -6,3 +6,21 @@ def missingNumbers(nums):
         if i not in numSet:
             res.append(i)
     return res
+
+
+# O(n) time | O(1) space
+def missingNumbersOptimal(nums):
+    numsSum = sum(nums)
+    total = sum(range(1, len(nums)+3))
+    missingTotal = total - numsSum
+    avg = missingTotal // 2
+    expectedRight = sum(range(avg+1, len(nums)+3))
+    expectedLeft = sum(range(1, avg+1))
+    leftSum = 0
+    rightSum = 0
+    for num in nums:
+        if num <= avg:
+            leftSum += num
+        else: 
+            rightSum += num
+    return [expectedLeft-leftSum, expectedRight-rightSum]
