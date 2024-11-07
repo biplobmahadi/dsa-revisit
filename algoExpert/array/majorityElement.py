3cc4db7e62954cc658efa3c4fc8cd3dba7f36182
Biplob <biplobhasan547@gmail.com>
2024-11-07 11:58:25 +0600

    add optimal soln to get mejority element in array

--- algoExpert/array/majorityElement.py
+++ algoExpert/array/majorityElement.py
@@ -8,3 +8,18 @@ def majorityElement(array):
         if key > res[0]:
             res = [key, val]
     return res[1]
+
+# O(n) time | O(1) space
+def majorityElementOptimal(array):
+    res = array[0]
+    count = 1
+
+    for i in range(1, len(array)):
+        if count == 0:
+            res = array[i]
+            
+        if res == array[i]:
+            count += 1
+        else:
+            count -= 1
+    return res
