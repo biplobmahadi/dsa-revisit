ce0f8b569a363986c08c1f6ab9014161c0f341fa
biplob <biplobhasan547@gmail.com>
2025-01-18 14:09:11 +0600

    find sorted square array of a sorted arr

--- algoExpert/array/sortedSquareArr.py
+++ algoExpert/array/sortedSquareArr.py
@@ -9,3 +9,16 @@ def sortedSquaredArray(array):
             res[i] = array[left] * array[left]
             left += 1
     return res
+
+def sortedSquaredArray(array):
+    sqArr = []
+    l, r = 0, len(array)-1
+    while r>=l:
+        if abs(array[l]) > abs(array[r]):
+            sqArr.append(array[l] ** 2)
+            l+=1
+        else:
+            sqArr.append(array[r] ** 2)
+            r-=1
+    sqArr.reverse()
+    return sqArr
