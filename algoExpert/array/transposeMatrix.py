7fe690bd4ec16ddbe3617407172644fbfde17405
biplob <biplobhasan547@gmail.com>
2025-01-19 22:34:02 +0600

    transpose a matrix

--- algoExpert/array/transposeMatrix.py
+++ algoExpert/array/transposeMatrix.py
@@ -7,3 +7,13 @@ def transposeMatrix(matrix):
             eachRow.append(matrix[row][col])
         transposedMatrix.append(eachRow)
     return transposedMatrix
+
+def transposeMatrix2(matrix):
+    res = []
+    R, C = len(matrix), len(matrix[0])
+    for i in range(C):
+        newArr = []
+        for j in range(R):
+            newArr.append(matrix[j][i])
+        res.append(newArr)
+    return res
