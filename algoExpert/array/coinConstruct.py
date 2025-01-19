34b709c6e24b74a6c4fd223c1f606b929c52f00f
biplob <biplobhasan547@gmail.com>
2025-01-19 22:26:24 +0600

    construct amount with coin to check min which amount not possible to construct

--- algoExpert/array/coinConstruct.py
+++ algoExpert/array/coinConstruct.py
@@ -7,3 +7,13 @@ def nonConstructibleChange(coins):
             return amount + 1
         amount += coin
     return amount+1
+
+def nonConstructibleChange2(coins):
+    if not coins: return 1
+    coins.sort()
+    curr = 0
+    for n in coins:
+        if curr+1 < n:
+            return curr+1
+        curr += n
+    return curr+1
