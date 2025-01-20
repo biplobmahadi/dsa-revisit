86b9db28d180dacbb9cf7a24c4fedbbd670cdd98
biplob <biplobhasan547@gmail.com>
2025-01-20 23:45:51 +0600

    find triplets of 3 number sum

--- algoExpert/array/threeSum.py
+++ algoExpert/array/threeSum.py
@@ -15,3 +15,21 @@ def threeNumberSum(array, targetSum):
                 left += 1
                 right -= 1
     return triplets
+
+def threeNumberSum2(array, targetSum):
+    res = []
+    array.sort()
+    for i in range(len(array)-2):
+        l, r = i+1, len(array)-1
+        while r > l:
+            total = array[i] + array[l] + array[r]
+            if total > targetSum:
+                r -= 1
+            elif total < targetSum:
+                l += 1
+            else:
+                res.append([array[i], array[l], array[r]])
+                l+=1
+                r-=1
+    return res
+            
