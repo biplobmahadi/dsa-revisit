b4868551523f9dbdec30469e07fab6c945dd3674
Biplob <biplobhasan547@gmail.com>
2025-01-24 15:31:16 +0600

    optimize soln of max window element

--- neet75/slidingWindow/maxInWindow.py
+++ neet75/slidingWindow/maxInWindow.py
@@ -7,3 +7,20 @@ class Solution:
             res.append(ans)
             l+=1
         return res
+
+class Solution2:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        q = deque()
+        l, r = 0, 0
+        while r < len(nums):
+            while q and nums[r] > nums[q[-1]]:
+                q.pop()
+            q.append(r)
+            if r - k+1 >= 0:
+                res.append(nums[q[0]])
+                if q[0] == l:
+                    q.popleft()
+                l+=1
+            r+=1
+        return res
