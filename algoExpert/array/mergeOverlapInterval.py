7701fe753decee507a399505a8a2dc7d5b20be9f
biplob <biplobhasan547@gmail.com>
2025-02-03 01:02:22 +0600

    merge ovelapped intervals

--- algoExpert/array/mergeOverlapInterval.py
+++ algoExpert/array/mergeOverlapInterval.py
@@ -11,3 +11,17 @@ def mergeOverlappingIntervals(intervals):
             curr = intervals[i]
     nonIntervals.append(curr)
     return nonIntervals
+
+def mergeOverlappingIntervals(intervals):
+    intervals.sort()
+    res = []
+    prev = intervals[0]
+    for i in range(1, len(intervals)):
+        curr = intervals[i]
+        if curr[0] <= prev[1]:
+            prev = [min(curr[0], prev[0]), max(curr[1], prev[1])]
+        else:
+            res.append(prev)
+            prev = curr
+    res.append(prev)
+    return res
