f8f5feeddd38fedf9c286d7d1f82e8c34c608a29
biplob <biplobhasan547@gmail.com>
2025-02-02 17:05:34 +0600

    add optimal soln to get largest rectangle using stack

--- neet75/stack/largestRectangle.py
+++ neet75/stack/largestRectangle.py
@@ -9,3 +9,17 @@ class Solution:
                 r+= 1
             res = max((r-l+1)*heights[i], res)
         return res
+    
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        res, stack = 0, []
+        for i, h in enumerate(heights):
+            curr = i
+            while stack and stack[-1][1] > h:
+                iS, hS = stack.pop()
+                curr = iS
+                res = max(res, hS*(i-iS))
+            stack.append((curr, h))
+        for i, h in stack:
+            res = max(res, h*(len(heights)-i))
+        return res
