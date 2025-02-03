e818d3a8be16bd91561bcdedcc1a251c7b98f09b
biplob <biplobhasan547@gmail.com>
2025-02-03 23:44:28 +0600

    add function to find missing numbers in an array

--- algoExpert/array/missingNum.py
+++ algoExpert/array/missingNum.py
@@ -24,3 +24,23 @@ def missingNumbersOptimal(nums):
         else: 
             rightSum += num
     return [expectedLeft-leftSum, expectedRight-rightSum]
+
+
+def missingNumbers2(nums):
+    curr = sum(nums)
+    total = 0
+    for i in range(1, len(nums)+3):
+        total += i
+    need = total - curr
+    avg = need // 2
+    avgTotal = 0
+    avgCurr = 0
+    for n in nums:
+        if n <= avg:
+            avgCurr+= n
+    for i in range(1, avg+1):
+        avgTotal += i
+    one = avgTotal - avgCurr
+    res = [one, need-one]
+    return res
+    
