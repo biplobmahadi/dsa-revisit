e3d8b0093adc01e51956da4b7b23ff2b20c07258
Biplob <biplobhasan547@gmail.com>
2025-02-26 16:06:26 +0600

    add subarraySort2 function to identify unsorted subarray boundaries

--- algoExpert/array/subarraySort.py
+++ algoExpert/array/subarraySort.py
@@ -19,4 +19,25 @@ def subarraySort(array):
     while maxIn <= array[right]:
         right -=1
     return [left, right]
+
+def notOutside(i, arr):
+    if i == 0:
+        return arr[i+1] < arr[i]
+    if i == len(arr)-1:
+        return arr[i-1] > arr[i]
+    return arr[i-1] > arr[i] or arr[i+1] < arr[i]
     
+def subarraySort2(array):
+    minVal, maxVal = float('inf'), float('-inf')
+    for i in range(len(array)):
+        if notOutside(i, array):
+            minVal, maxVal = min(minVal, array[i]), max(maxVal, array[i])
+
+    if minVal == float('inf'):
+        return [-1, -1]
+    l, r = 0, len(array)-1
+    while array[l] <= minVal:
+        l+=1
+    while array[r] >= maxVal:
+        r-=1
+    return [l, r]
