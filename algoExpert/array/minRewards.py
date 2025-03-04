6dccc05e01efe466e4b77734abb6fe2dad3a8f43
Biplob <biplobhasan547@gmail.com>
2025-03-04 14:07:02 +0600

    add minRewards2 function to calculate minimum rewards based on scores

--- algoExpert/array/minRewards.py
+++ algoExpert/array/minRewards.py
@@ -9,3 +9,14 @@ def minRewards(scores):
             rewards[i] = max(rewards[i], rewards[i+1] + 1)
             
     return sum(rewards)
+
+
+def minRewards2(scores):
+    res = [1] * len(scores)
+    for i in range(1, len(scores)):
+        if scores[i] > scores[i-1]:
+            res[i] = res[i-1]+1
+    for i in reversed(range(len(scores)-1)):
+        if scores[i] > scores[i+1]:
+            res[i] = max(res[i], res[i+1]+1)
+    return sum(res)
