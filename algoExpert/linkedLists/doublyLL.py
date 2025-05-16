7d0f0dbc20a8baaf65caf5c24627172f6ceb858a
biplob <biplobhasan547@gmail.com>
2025-05-16 13:40:50 +0600

    add DoublyLinkedList class with methods for insertion, removal, and value checking

---
+++ algoExpert/linkedLists/doublyLL.py
@@ -0,0 +1,92 @@
+# This is an input class. Do not edit.
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+# Feel free to add new properties and methods to the class.
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = self.head
+            return
+        self.insertBefore(self.head, node)
+
+    def setTail(self, node):
+        if not self.tail:
+            self.setHead(node)
+            return
+        self.insertAfter(self.tail, node)
+
+    def insertBefore(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return 
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node.prev
+        nodeToInsert.next = node
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
+
+    def insertAfter(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node
+        nodeToInsert.next = node.next
+        if node.next is None:
+            self.tail = nodeToInsert
+        else:
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
+
+    def insertAtPosition(self, position, nodeToInsert):
+        if position == 1:
+            self.setHead(nodeToInsert)
+            return
+        node = self.head
+        n = 1
+        while node and position != n:
+            node = node.next
+            n+=1
+        if node:
+            self.insertBefore(node, nodeToInsert)
+        else:
+            self.setTail(nodeToInsert)
+
+    def removeNodesWithValue(self, value):
+        curr = self.head
+        while curr:
+            node = curr
+            curr = curr.next
+            if value == node.value:
+                self.remove(node)
+
+    def remove(self, node):
+        if node == self.head:
+            self.head = self.head.next
+        if node == self.tail:
+            self.tail = self.tail.prev
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.prev = None
+        node.next = None
+
+        
+    def containsNodeWithValue(self, value):
+        curr = self.head
+        while curr and curr.value != value:
+            curr = curr.next
+        return curr is not None
+            
